68a40816b6a5112016c5f68eadbf45ec6e2c0659
Bilal <bilal.a.haroon@gmail.com>
2018-10-25 19:17:58 -0500

    got api to frontend

--- backend/volunteerapi/login/views.py
+++ backend/volunteerapi/login/views.py
@@ -13,5 +13,9 @@ class login(APIView):
 
         return Response(serializer.data)
 
-
-        print(serializer.data)
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var element = <h1>Hello World</h1>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+fetch("http://127.0.0.1:8000/users/")
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    console.log(data[0].name);
+  });
+
+ReactDOM.render(element, document.getElementById("root"));
